f99cea0fcf3b872c2e586aeaf988ee4e95359d38
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2022-07-26 22:23:39 +0530

    create user RBAC

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/store'), ProductModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/store'), ProductModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
