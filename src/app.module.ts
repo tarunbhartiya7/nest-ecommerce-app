faa60541b4d9d858ceb3bfa8d317853ad60ba277
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2022-08-09 13:35:11 +0530

    cart logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/store'), ProductModule, UserModule, AuthModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/store'), ProductModule, UserModule, AuthModule, CartModule],
   controllers: [AppController],
   providers: [AppService],
 })
